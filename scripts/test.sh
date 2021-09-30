747799025a029d5d1cb5374d11c499c2e814ad64
Santiago Palladino <spalladino@gmail.com>
2021-09-30 16:46:18 -0300

    Batch files when running them through yajsv

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 
 echo "Testing contract file names..."
 for FILE in contracts/**/*json; do
-  if [[ ! $FILE =~ ^contracts/[0-9]+/0x[A-Za-z0-9]{40}\.json$ ]]; then
+  if [[ ! $FILE =~ ^contracts/[0-9]+/0x[a-z0-9]{40}\.json$ ]]; then
     echo "Wrong filename format: $FILE"
     exit 1
   fi
 done
 
 echo; echo "Testing contract entries..."
-~/go/bin/yajsv -s schemas/contract.json contracts/**/*.json
+echo contracts/**/*.json | xargs -n 1000 ~/go/bin/yajsv -q -s schemas/contract.json
 
 echo; echo "Testing project entries..."
 ~/go/bin/yajsv -s schemas/project.json projects/*.json
