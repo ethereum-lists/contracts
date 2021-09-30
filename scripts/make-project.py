dc7264dede6767229209126079c690aa70527474
Santiago Palladino <spalladino@gmail.com>
2021-09-30 17:02:22 -0300

    Add make-project script and test project field in contract entries

---
+++ scripts/make-project.py
@@ -0,0 +1,29 @@
+from sys import argv
+
+import os
+import json
+
+def makeproject(project):
+  if not project: return None
+  name = " ".join(project.split("_")).title()
+  return { "name": name }
+
+
+def main():
+  projects = argv[1:]
+  for project in projects:
+    path = f"projects/{project}.json"
+    if os.path.exists(path): continue
+    
+    data = makeproject(project)
+    if not data: continue
+    with open(path, 'w') as outfile: 
+      json.dump(data, outfile, indent=2)
+      print(f" written {path}")
+
+if __name__ == "__main__":
+  try:
+    main()
+  except Exception as err:
+    print("Error:", err)
+    exit(1)
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 set -euo pipefail
 
+CHAINID="**"
+
 echo "Testing contract file names..."
-for FILE in contracts/**/*json; do
+for FILE in contracts/$CHAINID/*json; do
   if [[ ! $FILE =~ ^contracts/[0-9]+/0x[a-z0-9]{40}\.json$ ]]; then
-    echo "Wrong filename format: $FILE"
+    echo " Wrong filename format: $FILE"
     exit 1
   fi
 done
+echo " OK"
 
 echo; echo "Testing contract entries..."
-echo contracts/**/*.json | xargs -n 1000 ~/go/bin/yajsv -q -s schemas/contract.json
+echo contracts/$CHAINID/*.json | xargs -n 1000 ~/go/bin/yajsv -q -s schemas/contract.json
+echo " OK"
 
 echo; echo "Testing project entries..."
-~/go/bin/yajsv -s schemas/project.json projects/*.json
+~/go/bin/yajsv -q -s schemas/project.json projects/*.json
+echo " OK"
 
-echo; echo "Success"
+echo; echo "Testing project names in contract entries..."
+for PROJECT in $(echo contracts/$CHAINID/*.json | xargs -n 1000 jq -r '.project' | sort | uniq); do
+  if [[ ! -f "projects/$PROJECT.json" ]]; then
+    echo " Missing project entry: $PROJECT"
+    exit 2
+  fi
+done
+
+echo " OK"
